7005e904cefb6cb714499a1e51a68d5b38c5b49d
agusCEV12 <115225416+agusCEV12@users.noreply.github.com>
2022-11-10 20:46:04 +0100

    Login hecho // con error password 10/11

--- app/src/main/java/com/example/alfred/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/alfred/ui/login/LoginActivity.java
@@ -3,14 +3,11 @@ package com.example.alfred.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
@@ -24,14 +21,10 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //private LoginViewModel loginViewModel;
-   // private ActivityLoginBinding binding;
-
     private EditText userName, password;
     private String strUserName, strPassword;
     //URL del archivo php de nuestro LOGIN
     private static final String URL1="http://192.168.0.14/alfred/login.php";
-    private Button btnLogin;
 
 
     @Override
@@ -49,12 +42,13 @@ public class LoginActivity extends AppCompatActivity {
     public void goToRegister (View view) {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+        finish();
     }
 
 
     public void goToHome (View view){
         // ponemos logica de login
-        // si no se ha introducido correo o correo
+        // si no se ha introducido correo
         if (userName.getText().toString().equals("")) {
             Toast.makeText(this, "Debes de introducir un correo", Toast.LENGTH_SHORT).show();
         } else if(password.getText().toString().equals("")){
@@ -75,8 +69,8 @@ public class LoginActivity extends AppCompatActivity {
                     userName.setText("");
                     password.setText("");
                     //Mandamos a la actividad de Home
-
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                    Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
                 }
@@ -84,9 +78,8 @@ public class LoginActivity extends AppCompatActivity {
                 progressDialog.dismiss();
                 Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }){
-                @Nullable
                 @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
+                protected Map<String, String> getParams() {
 
                     Map<String,String> params = new HashMap<>();
                     params.put("email", strUserName);
