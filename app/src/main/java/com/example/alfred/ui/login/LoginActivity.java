84f30b3d466a6710a2b1004f34a32fc2784ac20e
agusCEV12 <115225416+agusCEV12@users.noreply.github.com>
2022-11-15 19:29:32 +0100

    Login hecho !!! :)

--- app/src/main/java/com/example/alfred/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/alfred/ui/login/LoginActivity.java
@@ -3,18 +3,21 @@ package com.example.alfred.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.alfred.R;
 import com.example.alfred.ui.HomeActivity;
-import com.example.alfred.ui.recovery_pass.RecoverPassActivity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,8 +26,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText userName, password;
     private String strUserName, strPassword;
+
     //URL del archivo php de nuestro LOGIN
-    private static final String URL1="http://192.168.0.14/alfred/login.php";
+    private static final String URL2="http://192.168.0.14/alfred/login.php";
+
 
 
     @Override
@@ -46,65 +51,68 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    public void goToHome (View view){
-        // ponemos logica de login
-        // si no se ha introducido correo
-        if (userName.getText().toString().equals("")) {
-            Toast.makeText(this, "Debes de introducir un correo", Toast.LENGTH_SHORT).show();
-        } else if(password.getText().toString().equals("")){
-            Toast.makeText(this, "Debes de introducir la contraseña", Toast.LENGTH_SHORT).show();
-        } else {
-            //salga este mensaje mientras se espera
+    public void goToForgotPassword (View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToHome (View view) {
+        if(userName.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.getText().toString().equals("")){
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
             final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setMessage("Por favor espere");
+            progressDialog.setMessage("Por favor espera...");
             progressDialog.show();
 
             strUserName = userName.getText().toString().trim();
             strPassword = password.getText().toString().trim();
 
-            StringRequest request = new StringRequest(Request.Method.POST, URL1, response -> {
-                progressDialog.dismiss();
 
-                if (response.equalsIgnoreCase("Login Correcto")) {
-                    userName.setText("");
-                    password.setText("");
-                    //Mandamos a la actividad de Home
-                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                    Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
-                }
-            }, error -> {
-                progressDialog.dismiss();
-                Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }){
+            StringRequest request = new StringRequest(Request.Method.POST, URL2, new Response.Listener<String>() {
                 @Override
-                protected Map<String, String> getParams() {
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
 
+                    if(response.equalsIgnoreCase("Login Correcto")){
+                        userName.setText("");
+                        password.setText("");
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        //startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        //startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        //Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+            },new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
                     Map<String,String> params = new HashMap<>();
-                    params.put("email", strUserName);
-                    params.put("password", strPassword);
-
+                    params.put("email",strUserName);
+                    params.put("password",strPassword);
                     return params;
+
                 }
             };
 
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
             requestQueue.add(request);
-
         }
     }
-
-
-    public void goToForgotPassword (View view) {
-        Intent intent = new Intent(this, RecoverPassActivity.class);
-        startActivity(intent);
-    }
 }
-
-  /*   NOSE  SI VALE
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        Toast.makeText(LoginActivity.this, "Vamos a la app", Toast.LENGTH_SHORT).show();
-        //Terminar la actividad si se entra bien
-        finish();*/
